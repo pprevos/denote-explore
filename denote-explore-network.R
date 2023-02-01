cdfd60209d09980ca24d947d14caccb1a0be9a72
Peter Prevos <peter@prevos.net>
2023-02-01 20:41:53 +1100

    Network version in R

--- denote-explore-network.R
+++ denote-explore-network.R
@@ -3,10 +3,10 @@
 # Install required packages
 req_packages <- c("stringr","dplyr", "igraph", "networkD3", "htmlwidgets")
 for (i in req_packages) { #Installs packages if not yet installed
-    if(! i%in% row.names(installed.packages())) install.packages(i)
+  if(! i%in% row.names(installed.packages())) install.packages(i)
 }
 
-## Initialise libraries
+## Initialise libraries.
 library(stringr)     # String manipulation
 library(dplyr)       # Data manipulation
 library(igraph)      # Network analysis
@@ -17,49 +17,46 @@ library(htmlwidgets) # Save webpage to disk
 denote_directory <- "~/Documents/notes"
 
 ## List denote files
-## Only Org files for now
-files <- list.files(denote_directory,
-                    recursive = TRUE,
-                    full.names = TRUE,
-                    pattern = ".org")
-files <- files[!dir.exists(files)]
+denote_files <- list.files(denote_directory,
+                           recursive = TRUE,
+                           full.names = TRUE,
+                           pattern = "[0-9]{8}T[0-9]{6}--.*__.*")
 
 ## Create table of files from filenames
-denotes <- tibble(filename   = files,
+denotes <- tibble(filename   = denote_files,
                   identifier = str_extract(filename, "[0-9]{8}T[0-9]{6}"),
                   title      = str_extract(filename, "(?<=\\-\\-).*(?=__)"),
-                  tags       = str_extract(filename, "(?<=\\_\\_).*(?=\\.)")) %>%
-    mutate(title = str_replace_all(title, "-", " "),
-           title = str_to_title(title)) %>%
-    filter(!is.na(identifier))
+                  tags       = str_extract(filename, "(?<=\\_\\_).*(?=\\.)"),
+                  filetype   = str_extract(filename, "\\..*$")) %>%
+  mutate(title = str_replace_all(title, "-", " "),
+         title = str_to_title(title))
 
-# Read all files to harvest connections
+# Read all text files to harvest links
+text_denotes <- filter(denotes, filetype %in% c(".org", ".md", ".txt"))
+
 links <- list()
 
-for (f in denotes$filename) {
-    l <- str_extract(readLines(f), "(?<=denote:)[0-9]{8}T[0-9]{6}")
-    if (any(!is.na(l)))
-        links[[f]] <- data.frame(
-            from = str_extract(f, "[0-9]{8}T[0-9]{6}"),
-            to = l[!is.na(l)])
+for (f in text_denotes$filename) {
+  l <- str_extract(readLines(f), "(?<=denote:)[0-9]{8}T[0-9]{6}")
+  if (any(!is.na(l)))
+    links[[f]] <- data.frame(
+      from = str_extract(f, "[0-9]{8}T[0-9]{6}"),
+      to = l[!is.na(l)])
 }
 
 # Collapse and clean list
 network <- bind_rows(links)
 row.names(network) <- NULL
 
-# Remove empty links (non-org files)
-network <- network[!duplicated(network), ]
-
-# Remove any self-referencing links
-network <- network[network$from != network$to, ]
+# Remove empty duplicates and self-referencing links
+network <- network[!duplicated(network) | network$from == network$to, ]
 
 # Replace ids with names (remove links to attachments)
 network_names <- network %>%
-    left_join(denotes, by = c("from" = "identifier")) %>%
-    left_join(denotes, by = c("to" = "identifier")) %>%
-    select(from = title.x, to = title.y) %>%
-    filter(!is.na(to))
+  left_join(denotes, by = c("from" = "identifier")) %>%
+  left_join(denotes, by = c("to" = "identifier")) %>%
+  select(from = title.x, to = title.y) %>%
+  filter(!is.na(to))
 
 # Create network and visualise
 
@@ -77,3 +74,4 @@ p <- simpleNetwork(network_names,
 
 # Save HTML and JS to disk
 saveWidget(p, file = paste0("~/denote-network.html"))
+
